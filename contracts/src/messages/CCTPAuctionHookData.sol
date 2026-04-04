3a8e3c289b405b1a563a814aa882ab229e7920d9
lumoswiz <lumoswiz@protonmail.com>
2026-04-04 12:59:16 +0200

    feat: implement mintAndSubmitBid with CCTP mint and fallback transfer

--- contracts/src/messages/CCTPAuctionHookData.sol
+++ contracts/src/messages/CCTPAuctionHookData.sol
@@ -4,21 +4,18 @@ pragma solidity 0.7.6;
 import { TypedMemView } from "@memview-sol/contracts/TypedMemView.sol";
 import { AuctionParams } from "src/types/AuctionParams.sol";
 
-/**
- * @title CCTPAuctionHookData Library
- * @notice Library for parsing CCTPAuction hook data from CCTP burn messages
- *
- * @dev Hook data layout:
- *      Bytes 0-3:     bytes4  - Magic value (bytes4(keccak256("CCTPAuction.Version.0")) = 0xd55705b2)
- *      Bytes 4-7:     uint32  - Data length
- *      Bytes 8-27:    address - Auction contract address
- *      Bytes 28-59:   uint256 - maxPrice for the bid
- *      Bytes 60-79:   address - Bidder (the user on whose behalf we submit the bid)
- *      Bytes 80-111:  uint256 - prevTickPrice for bid placement
- *      Bytes 112+:    bytes   - Inner hook data passed through to the auction contract
- * @dev Reverts if hook data is less than 112 bytes or magic value does not match.
- * @dev To upgrade, use bytes4(keccak256("CCTPAuction.Version.N")) for version N.
- */
+/// @title CCTPAuctionHookData
+/// @notice Library for parsing CCTPAuction bid parameters from CCTP burn message hook data
+/// @dev Hook data layout:
+///      Bytes 0-3:     bytes4  - Magic value (bytes4(keccak256("CCTPAuction.Version.0")) = 0xd55705b2)
+///      Bytes 4-7:     uint32  - Data length
+///      Bytes 8-27:    address - Auction contract address
+///      Bytes 28-59:   uint256 - maxPrice for the bid
+///      Bytes 60-79:   address - Bidder (the user on whose behalf we submit the bid)
+///      Bytes 80-111:  uint256 - prevTickPrice for bid placement
+///      Bytes 112+:    bytes   - Inner hook data passed through to the auction contract
+/// @dev Reverts if hook data is less than 112 bytes or magic value does not match.
+/// @dev To upgrade, use bytes4(keccak256("CCTPAuction.Version.N")) for version N.
 library CCTPAuctionHookData {
     using TypedMemView for bytes29;
 
@@ -37,13 +34,11 @@ library CCTPAuctionHookData {
 
     uint8 private constant MIN_HOOK_LENGTH = 112;
 
-    /**
-     * @notice Parse auction bid parameters from hook data
-     * @dev Reverts if hook data is less than 112 bytes or magic value does not match.
-     * @dev Bytes 4-7 (data length) are not validated onchain but may be used offchain.
-     * @param _hookData Hook data from the CCTP burn message
-     * @return _params Auction params required to submit a bid for the user
-     */
+    /// @notice Parse auction bid parameters from hook data
+    /// @dev Reverts if hook data is less than 112 bytes or magic value does not match.
+    /// @dev Bytes 4-7 (data length) are not validated onchain but may be used offchain.
+    /// @param _hookData Hook data from the CCTP burn message
+    /// @return _params Auction params required to submit a bid for the user
     function _parseBidParameters(bytes29 _hookData) internal pure returns (AuctionParams memory _params) {
         uint256 hookLength = _hookData.len();
         require(hookLength >= MIN_HOOK_LENGTH, "CCTPAuctionHookData: too short");
@@ -52,16 +47,16 @@ library CCTPAuctionHookData {
         require(magic == MAGIC, "CCTPAuctionHookData: invalid magic");
 
         _params = AuctionParams({
-            _auction: _hookData.indexAddress(AUCTION_INDEX),
-            _maxPrice: _hookData.indexUint(MAX_PRICE_INDEX, MAX_PRICE_LENGTH),
-            _bidder: _hookData.indexAddress(BIDDER_INDEX),
-            _prevTickPrice: _hookData.indexUint(PREV_TICK_PRICE_INDEX, PREV_TICK_PRICE_LENGTH),
-            _innerHookData: bytes29(0)
+            auction: _hookData.indexAddress(AUCTION_INDEX),
+            maxPrice: _hookData.indexUint(MAX_PRICE_INDEX, MAX_PRICE_LENGTH),
+            bidder: _hookData.indexAddress(BIDDER_INDEX),
+            prevTickPrice: _hookData.indexUint(PREV_TICK_PRICE_INDEX, PREV_TICK_PRICE_LENGTH),
+            innerHookData: bytes29(0)
         });
 
         uint256 _innerLength = hookLength - INNER_HOOK_DATA_INDEX;
         if (_innerLength > 0) {
-            _params._innerHookData = _hookData.slice(INNER_HOOK_DATA_INDEX, _innerLength, 0);
+            _params.innerHookData = _hookData.slice(INNER_HOOK_DATA_INDEX, _innerLength, 0);
         }
     }
 }
--- contracts/src/types/AuctionParams.sol
+++ contracts/src/types/AuctionParams.sol
@@ -1,10 +1,16 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.7.6;
 
+/// @notice Parsed bid parameters extracted from CCTP burn message hook data
+/// @param auction The auction contract to submit the bid to
+/// @param maxPrice The maximum price the bidder is willing to pay
+/// @param bidder The user on whose behalf the bid is submitted
+/// @param prevTickPrice The previous tick price for bid placement ordering
+/// @param innerHookData Opaque hook data passed through to the auction contract
 struct AuctionParams {
-    address _auction;
-    uint256 _maxPrice;
-    address _bidder;
-    uint256 _prevTickPrice;
-    bytes29 _innerHookData;
+    address auction;
+    uint256 maxPrice;
+    address bidder;
+    uint256 prevTickPrice;
+    bytes29 innerHookData;
 }
